b09b61b5c9393e52bd2e11706cd91c21720930f1
melt7777 <melt@getpimp.org>
2019-04-17 21:57:10 -0400

    fixed to use specified, default, or git version of pimpminers.conf

--- pimpminersconf.go
+++ pimpminersconf.go
@@ -10,7 +10,7 @@ Copyright (c) 2019 getPiMP.org.  All Rights Reserved.
 Author: Portable Instant Mining Platform, LLC
 License: This code is licensed for use with PiMP only.
 Description: PiMP OS pimpminers.conf API wrapper in Golang
-Interacts with this file: https://github.com/melt7777/pimpminers-conf/pimpminers.conf
+Interacts with this file: https://github.com/getpimp/pimpminers-conf/pimpminers.conf
 ==================================================================
 */
 package pimpminersconf
@@ -18,10 +18,14 @@ package pimpminersconf
 import (
 	"bytes"
 	"encoding/json"
-	"fmt"
+	"net/http"
+	"io"
 	"io/ioutil"
 	"os"
 )
+
+const REMOTE = "https://raw.githubusercontent.com/getpimp/pimpminers-conf/master/pimpminers.conf"
+const LOCAL = "/opt/pimp/pimpminers.conf"
 
 type PimpMiner struct {
 	Info           string             `json:"info"`
@@ -47,14 +51,22 @@ type PimpMinerProfile struct {
 	Cfile string `json:"cfile"`
 }
 
-// Init returns a mapstring of PimpMiners populated with data from the pimpminers.conf file
-func Init(file string) map[string][]PimpMiner {
+// Load returns a mapstring of PimpMiners populated with data from the pimpminers.conf file
+func Load(file string) map[string][]PimpMiner {
+	// if no file specified, use default in /opt/pimp.	
 	if file == "" {
-		file = "/opt/pimp/pimpminers.conf"
+		file = LOCAL
 	}
+	if FileExists(LOCAL) != "" {
+		// download the file 
+		if err := DownloadFile(file, REMOTE); err != nil {
+			panic(err)
+		}
+	}
+	
 	jsonFile, err := os.Open(file) // Open the JSON file
 	if err != nil {                // if os.Open returns an error then handle it
-		fmt.Println(err)
+		panic(err)
 	}
 	defer jsonFile.Close()                   // defer the closing of our jsonFile so that we can parse it later on
 	byteValue, _ := ioutil.ReadAll(jsonFile) // read our opened json file as a byte array.
@@ -74,8 +86,36 @@ func GetMiner(name string, miners map[string][]PimpMiner) PimpMiner {
 	return PimpMiner{}
 }
 
-// JsonPrettyPrint takes a string of json in and returns a prettier string of json out.
-func JsonPrettyPrint(in string) string {
+// DownloadFile will download a url to a local file.
+func DownloadFile(filepath string, url string) error {
+    // Get the data
+    resp, err := http.Get(url)
+    if err != nil {
+        return err
+    }
+    defer resp.Body.Close()
+    // Create the file
+    out, err := os.Create(filepath)
+    if err != nil {
+        return err
+    }
+    defer out.Close()
+    // Write the body to file
+    _, err = io.Copy(out, resp.Body)
+    return err
+}
+
+// FileExists takes a filename string and returns it if it exists, or empty string if it does not.
+func FileExists(file string) string {
+	if _, err := os.Stat(file); !os.IsNotExist(err) {
+		// path/to/whatever exists
+		return file
+	}
+	return ""
+}
+
+// PrettyPrint takes a string of json in and returns a prettier string of json out.
+func PrettyPrint(in string) string {
 	var out bytes.Buffer
 	err := json.Indent(&out, []byte(in), "", "\t")
 	if err != nil {
